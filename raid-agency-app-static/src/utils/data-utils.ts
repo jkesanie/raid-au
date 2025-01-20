4959cb05c406cb614bbf1401a97d0ddf201ff9c9
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2025-01-21 07:22:38 +1100

    sync: v2 => v3

--- raid-agency-app-static/src/utils/data-utils.ts
+++ raid-agency-app-static/src/utils/data-utils.ts
@@ -1,13 +1,13 @@
-export function getRaidAppUrl(hostname: string): string {
+export function getRaidAppUrl(): string {
   const localStaticPort = 4321;
-
-  const environment = hostname.includes("test")
+  const apiEndpoint = import.meta.env.API_ENDPOINT;
+  const environment = apiEndpoint.includes("test")
     ? "test"
-    : hostname.includes("demo")
+    : apiEndpoint.includes("demo")
       ? "demo"
-      : hostname.includes("prod")
+      : apiEndpoint.includes("prod")
         ? "prod"
-        : hostname.includes("stage")
+        : apiEndpoint.includes("stage")
           ? "stage"
           : "dev";
 
