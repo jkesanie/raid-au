81a4093ab557e1644e71728e9e6f4b704888f39e
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2025-01-24 18:10:04 +1100

    refactor: remove unused ORCID update logic and related imports from RaidDisplay component

--- raid-agency-app/src/pages/raid-display/RaidDisplay.tsx
+++ raid-agency-app/src/pages/raid-display/RaidDisplay.tsx
@@ -10,7 +10,6 @@ import {
   RawDataDisplay,
 } from "@/pages/raid-display/components";
 import { fetchRaid } from "@/services/raid";
-import { getApiEndpoint } from "@/utils/api-utils/api-utils";
 import { displayItems } from "@/utils/data-utils/data-utils";
 import {
   DocumentScanner as DocumentScannerIcon,
@@ -20,15 +19,11 @@ import {
 import { Box, Container, Stack } from "@mui/material";
 import { useKeycloak } from "@react-keycloak/web";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export const RaidDisplay = () => {
-  const endpoint = getApiEndpoint();
   const { keycloak, initialized } = useKeycloak();
   const { prefix, suffix } = useParams() as { prefix: string; suffix: string };
-  const [searchParams] = useSearchParams();
-  const verify = searchParams.get("verify");
   const handle = `${prefix}/${suffix}`;
 
   const readQuery = useQuery<RaidDto>({
@@ -40,49 +35,6 @@ export const RaidDisplay = () => {
       }),
     enabled: initialized && keycloak.authenticated,
   });
-
-  const updateOrcidStatus = async (verify: string) => {
-    try {
-      const [orcid, uuid] = atob(verify).split("|");
-      if (!readQuery.data?.contributor) return;
-      const contributors = readQuery.data.contributor;
-      for (const contributor of contributors) {
-        if (contributor.uuid === uuid) {
-          contributor.id = orcid;
-          contributor.status = "VERIFIED";
-        } else {
-          contributor.id = contributor.id || "";
-        }
-      }
-
-      const response = await fetch(`${endpoint}/raid/${prefix}/${suffix}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${keycloak.token}`,
-        },
-        body: JSON.stringify({
-          contributor: contributors,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      window.location.href = window.location.pathname;
-      return data;
-    } catch (error) {
-      console.error("Error updating ORCID status:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (verify && readQuery.data) {
-      updateOrcidStatus(verify);
-    }
-  }, [verify, readQuery.data, prefix, suffix, endpoint, keycloak.token]);
 
   if (readQuery.isPending) {
     return (
