1146b60de385694efcf6a0688e5214e2b32e3b75
Rob Leney <rob.leney@ardc.edu.au>
2025-02-03 16:33:46 +1100

    Add logging

--- api-svc/raid-api/src/main/java/au/org/raid/api/service/RaidListenerService.java
+++ api-svc/raid-api/src/main/java/au/org/raid/api/service/RaidListenerService.java
@@ -4,11 +4,13 @@ import au.org.raid.api.dto.ContributorStatus;
 import au.org.raid.api.factory.RaidListenerMessageFactory;
 import au.org.raid.idl.raidv2.model.Contributor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RaidListenerService {
@@ -19,10 +21,15 @@ public class RaidListenerService {
 
         for (final var contributor : contributors) {
             if (contributor.getEmail() != null) {
+                log.debug("Looking up contributor by email {}", contributor.getEmail());
+
                 final var response = orcidIntegrationClient.get(contributor.getEmail());
 
                 if (response.isPresent()) {
+
                     final var lookupResponse = response.get();
+
+                    log.debug("Received look up response: {uuid: {}, status: {}", lookupResponse.getUuid(), lookupResponse.getOrcidStatus());
 
                     if (lookupResponse.getOrcidStatus().equalsIgnoreCase("AUTHENTICATED")) {
                         contributor.uuid(lookupResponse.getUuid())
