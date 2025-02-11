36215eedbf72fa94c7ea6ceb45a8aa4a9d087034
Rob Leney <rob.leney@ardc.edu.au>
2025-02-11 13:09:04 +1100

    add https://app3.stage.raid.org.au as valid origin to GroupController

--- iam/src/main/java/au/org/raid/iam/provider/group/GroupController.java
+++ iam/src/main/java/au/org/raid/iam/provider/group/GroupController.java
@@ -31,6 +31,7 @@ public class GroupController {
             "https://app.demo.raid.org.au",
             "https://app3.demo.raid.org.au",
             "https://app.stage.raid.org.au",
+            "https://app3.stage.raid.org.au",
             "https://app.prod.raid.org.au"
     );
 
