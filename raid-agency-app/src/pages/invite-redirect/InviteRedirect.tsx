740c5711dc543c359a91e279eff037d89f50e785
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2025-02-20 11:21:41 +1100

    feat: refactor InviteRedirect to accept invites automatically and remove reject functionality

--- raid-agency-app/src/pages/invite-redirect/InviteRedirect.tsx
+++ raid-agency-app/src/pages/invite-redirect/InviteRedirect.tsx
@@ -1,31 +1,34 @@
 import { useSnackbar } from "@/components/snackbar";
 import { useKeycloak } from "@/contexts/keycloak-context";
-import { acceptInvite, rejectInvite } from "@/services/invite";
-import {
-  Button,
-  Card,
-  CardContent,
-  CardHeader,
-  Container,
-  Stack,
-} from "@mui/material";
+import { acceptInvite } from "@/services/invite";
+import { Container } from "@mui/material";
 import { useMutation } from "@tanstack/react-query";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Loading } from "../loading";
 
 export function InviteRedirect() {
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
   const handleBase64 = searchParams.get("handle");
-  const navigate = useNavigate();
   const { openSnackbar } = useSnackbar();
 
   const handle = handleBase64 ? atob(handleBase64) : "";
 
   const { token } = useKeycloak();
+  useEffect(() => {
+    if (code && token && handle) {
+      acceptInviteMutation.mutate({
+        code: code!,
+        token: token!,
+        handle,
+      });
+    }
+  }, []);
 
   const acceptInviteMutation = useMutation({
     mutationFn: acceptInvite,
-    onSuccess: (data, variables) => {
+    onSuccess: (data) => {
       if (data?.handle) {
         openSnackbar(
           `✅ You have accepted the invitation to join ${data.handle}. Redirecting now...`
@@ -33,23 +36,8 @@ export function InviteRedirect() {
         setTimeout(() => {
           // we can't use react-router's navigate here, because we need a page refresh to get the new token
           window.location.href = `/raids/${data.handle}`;
-        }, 3000);
+        }, 2000);
       }
-    },
-    onError: (error) => {
-      console.error("error", error);
-    },
-  });
-
-  const rejectInviteMutation = useMutation({
-    mutationFn: rejectInvite,
-    onSuccess: (data, variables) => {
-      openSnackbar(
-        `✅ You have rejected the invitation to join ${data.handle}. Redirecting now...`
-      );
-      setTimeout(() => {
-        navigate(`/raids`);
-      }, 3000);
     },
     onError: (error) => {
       console.error("error", error);
@@ -58,42 +46,7 @@ export function InviteRedirect() {
 
   return (
     <Container>
-      <Card>
-        <CardHeader
-          title="You are invited"
-          subheader="Please accept or reject the invitation"
-        />
-        <CardContent>
-          <Stack spacing={2} direction="row">
-            <Button
-              variant="outlined"
-              color="success"
-              onClick={() => {
-                acceptInviteMutation.mutate({
-                  code: code!,
-                  token: token!,
-                  handle,
-                });
-              }}
-            >
-              Accept
-            </Button>
-            <Button
-              variant="outlined"
-              color="error"
-              onClick={() => {
-                rejectInviteMutation.mutate({
-                  code: code!,
-                  token: token!,
-                  handle,
-                });
-              }}
-            >
-              Reject
-            </Button>
-          </Stack>
-        </CardContent>
-      </Card>
+      <Loading />
     </Container>
   );
 }
