fbb210d918adc3e6d955303b6af3ed72fd7384b7
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2025-03-18 21:26:57 +1100

    refactor: add file existence check in all-handles API to return empty array if file is missing

--- raid-agency-app-static/src/pages/api/all-handles.json.ts
+++ raid-agency-app-static/src/pages/api/all-handles.json.ts
@@ -4,6 +4,15 @@ import path from "node:path";
 export function GET() {
   // Path to your existing JSON file in the src directory
   const jsonPath = path.resolve("./src/raw-data/all-handles.json");
+  // check if the file exists and return empty array if not
+  if (!fs.existsSync(jsonPath)) {
+    return new Response("[]", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   const rawData = fs.readFileSync(jsonPath, "utf-8");
 
   return new Response(rawData, {
