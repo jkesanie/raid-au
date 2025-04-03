555851add47c6e34b0abeed0412b80d59732682f
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2025-04-03 15:51:36 +1100

    feat: add sitemap integration and update site URL in configuration

--- raid-agency-app-static/astro.config.mjs
+++ raid-agency-app-static/astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
+// todo: replace `prod` with actual value per environment
 export default defineConfig({
-  integrations: [tailwind()],
+  site: `https://static.prod.raid.org.au`,
+  integrations: [tailwind(), sitemap()],
 });
