df07823368efaaad845bfc082c640b9e489974b0
jkesanie <joonas.kesaniemi@gmail.com>
2025-05-20 08:15:01 +0300

    Fix test target field

--- api-svc/raid-api/src/intTest/java/au/org/raid/inttest/ContributorsIntegrationTest.java
+++ api-svc/raid-api/src/intTest/java/au/org/raid/inttest/ContributorsIntegrationTest.java
@@ -876,7 +876,7 @@ public class ContributorsIntegrationTest extends AbstractIntegrationTest {
                         assertThat(failures, hasSize(1));
                         assertThat(failures, contains(
                                 new ValidationFailure()
-                                        .fieldId("contributor[0]")
+                                        .fieldId("contributor[0].position")
                                         .errorType("notSet")
                                         .message("A contributor must have a position")
                         ));
