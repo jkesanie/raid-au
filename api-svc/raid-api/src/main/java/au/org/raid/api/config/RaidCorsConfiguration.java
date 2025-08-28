7ee960b59b544cffe260e4b102d36f0dc9d4dfb4
rob.leney <rob.leney@ardc.edu.au>
2025-08-28 16:36:56 +1000

    RAID-343: Add logging for cors configuration

--- api-svc/raid-api/src/main/java/au/org/raid/api/config/RaidCorsConfiguration.java
+++ api-svc/raid-api/src/main/java/au/org/raid/api/config/RaidCorsConfiguration.java
@@ -2,6 +2,7 @@ package au.org.raid.api.config;
 
 import au.org.raid.api.config.properties.CorsProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,7 +10,9 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class RaidCorsConfiguration {
@@ -19,8 +22,11 @@ public class RaidCorsConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
+        final var allowedOrigins = properties.getOrigins();
+        log.debug("setting CORS origins: {}", String.join(", ", allowedOrigins));
+
         // Add all allowed origins or use allowedOriginPatterns for wildcard support
-        configuration.setAllowedOrigins(properties.getOrigins());
+        configuration.setAllowedOrigins(allowedOrigins);
         // Include all necessary HTTP methods including OPTIONS
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         // Allow all common headers
