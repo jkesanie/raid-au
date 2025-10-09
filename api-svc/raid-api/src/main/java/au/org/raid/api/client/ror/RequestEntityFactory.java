209bfe5a09b608eb22cfabc8f8f2a50a6bea8b41
rob.leney <rob.leney@ardc.edu.au>
2025-10-09 17:25:27 +1100

    RAID-298: Add RorClient to retrieve organisation names for Datacite requests

---
+++ api-svc/raid-api/src/main/java/au/org/raid/api/client/ror/RequestEntityFactory.java
@@ -0,0 +1,25 @@
+package au.org.raid.api.client.ror;
+
+import au.org.raid.api.config.properties.RorClientProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+
+@Component
+@RequiredArgsConstructor
+public class RequestEntityFactory {
+    private final RorClientProperties properties;
+
+    public RequestEntity<Void> create(final String ror) {
+        final var id = ror.substring(ror.lastIndexOf("/") + 1);
+
+//get(properties.getBaseUrl() + "/organizations/" + id)
+
+        return RequestEntity.method(HttpMethod.GET, URI.create(properties.getBaseUrl() + "/organizations/" + id))
+                .header("Client-Id", properties.getClientId())
+                .build();
+    }
+}
---
+++ api-svc/raid-api/src/test/java/au/org/raid/api/client/ror/RequestEntityFactoryTest.java
@@ -0,0 +1,42 @@
+package au.org.raid.api.client.ror;
+
+import au.org.raid.api.config.properties.RorClientProperties;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpMethod;
+
+import java.net.URI;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RequestEntityFactoryTest {
+    @Mock
+    private RorClientProperties properties;
+    @InjectMocks
+    private RequestEntityFactory requestEntityFactory;
+
+    @Test
+    @DisplayName("Should return RequestEntity with valid input")
+    void returnsRequestEntity() {
+        final var baseUrl = "https://localhost";
+        final var clientId = "client-id";
+        final var ror = "https://ror.org/038sjwq14";
+
+        when(properties.getBaseUrl()).thenReturn(baseUrl);
+        when(properties.getClientId()).thenReturn(clientId);
+
+        final var request = requestEntityFactory.create(ror);
+
+        assertThat(request.getMethod(), is(HttpMethod.GET));
+        assertThat(request.getHeaders().get("Client-Id").get(0), is(clientId));
+        assertThat(request.getUrl(), is(URI.create("https://localhost/organizations/038sjwq14")));
+    }
+}
