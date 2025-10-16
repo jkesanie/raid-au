d703635d45e9047566baa64404766db4ea5cd224
rob.leney <rob.leney@ardc.edu.au>
2025-10-16 14:09:08 +1100

    RAID-298: Get name for Datacite publisher

--- api-svc/raid-api/src/test/java/au/org/raid/api/factory/datacite/DatacitePublisherFactoryTest.java
+++ api-svc/raid-api/src/test/java/au/org/raid/api/factory/datacite/DatacitePublisherFactoryTest.java
@@ -1,30 +1,43 @@
 package au.org.raid.api.factory.datacite;
 
+import au.org.raid.api.client.ror.RorClient;
 import au.org.raid.idl.raidv2.model.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class DatacitePublisherFactoryTest {
-    private final DatacitePublisherFactory publisherFactory = new DatacitePublisherFactory();
+    @Mock
+    private RorClient rorClient;
+    @InjectMocks
+    private DatacitePublisherFactory publisherFactory;
 
     @Test
     @DisplayName("Create sets all fields")
     void create() {
         final var id = "_id";
         final var schemaUri = "schema-uri";
+        final var name = "_name";
 
         final var owner = new Owner()
                 .id(id)
                 .schemaUri(schemaUri);
 
+        when(rorClient.getOrganisationName(id)).thenReturn(name);
+
         final var result = publisherFactory.create(owner);
 
         assertThat(result.getSchemeUri(), is(schemaUri));
         assertThat(result.getPublisherIdentifier(), is(id));
-        assertThat(result.getName(), is(id));
+        assertThat(result.getName(), is(name));
         assertThat(result.getPublisherIdentifierScheme(), is("ROR"));
     }
 }
